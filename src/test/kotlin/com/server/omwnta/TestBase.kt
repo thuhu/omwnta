a51550fe2b4c9c889178d0704680fc6119b74a45
Desmond <desmond@yellowprofessional.com>
2018-12-15 21:35:47 +0200

    Add profiles

--- src/test/kotlin/com/server/omwnta/TestBase.kt
+++ src/test/kotlin/com/server/omwnta/TestBase.kt
@@ -7,6 +7,6 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-//@ActiveProfiles({"test"})
+@ActiveProfiles("Test")
 class TestBase {
 }
